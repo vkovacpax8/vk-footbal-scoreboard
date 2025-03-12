1f49cad16c45b2750939b2c1ba6051f35016a2b9
Vladimir Kovac <vkovac@pax8.com>
2025-03-12 16:24:33 +0100

    Initial commit.

--- src/main/java/footbal/VKLiveFootballScoreboardApplication.java
+++ src/main/java/footbal/VKLiveFootballScoreboardApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VKLiveFootbalScoreboardApplication {
+public class VKLiveFootballScoreboardApplication {
     public static void main(String[] args) {
-        SpringApplication.run(VKLiveFootbalScoreboardApplication.class, args);
+        SpringApplication.run(VKLiveFootballScoreboardApplication.class, args);
     }
 }
